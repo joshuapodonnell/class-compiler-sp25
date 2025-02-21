2ecfc6e34ac212acb3466601a7d081aae7b373d6
Max Willsey <me@mwillsey.com>
2025-02-20 16:08:55 -0800

    lecture 10

--- lib/compile.ml
+++ lib/compile.ml
@@ -33,66 +33,64 @@ let lf_to_bool =
 
 let stack_offset (index : int) : operand = MemOffset (Reg Rsp, Imm index)
 
-let rec compile_exp (stack_index : int) (exp : s_exp) : directive list =
+let rec compile_exp (env : int symtab) (stack_index : int) (exp : s_exp) :
+    directive list =
   match exp with
   | Num n -> [ Mov (Reg Rax, operand_of_num n) ]
   | Sym "false" -> [ Mov (Reg Rax, operand_of_bool false) ]
   | Sym "true" -> [ Mov (Reg Rax, operand_of_bool true) ]
+  | Sym var -> [ Mov (Reg Rax, stack_offset (Symtab.find var env)) ]
+  | Lst [ Sym "let"; Lst [ Lst [ Sym var; e ] ]; e_body ] ->
+      compile_exp env stack_index e
+      @ [ Mov (stack_offset stack_index, Reg Rax) ]
+      @ compile_exp (Symtab.add var stack_index env) (stack_index - 8) e_body
   | Lst [ Sym "add1"; l ] ->
-      let p = compile_exp stack_index l in
+      let p = compile_exp env stack_index l in
       p @ [ Add (Reg Rax, operand_of_num 1) ]
   | Lst [ Sym "sub1"; l ] ->
-      let p = compile_exp stack_index l in
+      let p = compile_exp env stack_index l in
       p @ [ Sub (Reg Rax, operand_of_num 1) ]
   | Lst [ Sym "not"; l ] ->
-      compile_exp stack_index l
+      compile_exp env stack_index l
       @ [ Cmp (Reg Rax, operand_of_bool false) ]
       @ zf_to_bool
   | Lst [ Sym "zero?"; l ] ->
-      compile_exp stack_index l
+      compile_exp env stack_index l
       @ [ Cmp (Reg Rax, operand_of_num 0) ]
       @ zf_to_bool
   | Lst [ Sym "num?"; l ] ->
-      compile_exp stack_index l
+      compile_exp env stack_index l
       @ [ And (Reg Rax, Imm num_mask); Cmp (Reg Rax, Imm num_tag) ]
       @ zf_to_bool
   | Lst [ Sym "if"; e_cond; e_then; e_else ] ->
       let else_label = gensym "else" in
       let cont_label = gensym "continue" in
-      compile_exp stack_index e_cond
+      compile_exp env stack_index e_cond
       @ [ Cmp (Reg Rax, operand_of_bool false); Je else_label ]
-      @ compile_exp stack_index e_then
+      @ compile_exp env stack_index e_then
       @ [ Jmp cont_label ] @ [ Label else_label ]
-      @ compile_exp stack_index e_else
+      @ compile_exp env stack_index e_else
       @ [ Label cont_label ]
   | Lst [ Sym "+"; e1; e2 ] ->
-      compile_exp stack_index e1
-      @ [ Mov (stack_offset stack_index, Reg Rax) ]
-      @ compile_exp (stack_index - 8) e2
-      @ [ Mov (Reg R8, stack_offset stack_index) ]
-      @ [ Add (Reg Rax, Reg R8) ]
+      compile_binop env stack_index e1 e2 @ [ Add (Reg Rax, Reg R8) ]
   | Lst [ Sym "-"; e1; e2 ] ->
-      compile_exp stack_index e1
-      @ [ Mov (stack_offset stack_index, Reg Rax) ]
-      @ compile_exp (stack_index - 8) e2
-      @ [ Mov (Reg R8, Reg Rax) ] (* we have to flip the args for sub *)
-      @ [ Mov (Reg Rax, stack_offset stack_index) ]
-      @ [ Sub (Reg Rax, Reg R8) ]
+      compile_binop env stack_index e1 e2 @ [ Sub (Reg Rax, Reg R8) ]
   | Lst [ Sym "="; e1; e2 ] ->
-      compile_exp stack_index e1
-      @ [ Mov (stack_offset stack_index, Reg Rax) ]
-      @ compile_exp (stack_index - 8) e2
-      @ [ Mov (Reg R8, stack_offset stack_index) ]
+      compile_binop env stack_index e1 e2
       @ [ Cmp (Reg Rax, Reg R8) ]
       @ zf_to_bool
   | Lst [ Sym "<"; e1; e2 ] ->
-      compile_exp stack_index e1
-      @ [ Mov (stack_offset stack_index, Reg Rax) ]
-      @ compile_exp (stack_index - 8) e2
-      @ [ Mov (Reg R8, stack_offset stack_index) ]
+      compile_binop env stack_index e1 e2
       @ [ Cmp (Reg Rax, Reg R8) ]
       @ lf_to_bool
   | _ -> failwith "i dont know"
+
+and compile_binop env stack_index e1 e2 =
+  compile_exp env stack_index e1
+  @ [ Mov (stack_offset stack_index, Reg Rax) ]
+  @ compile_exp env (stack_index - 8) e2
+  @ [ Mov (Reg R8, Reg Rax) ] (* we have to flip the args for sub *)
+  @ [ Mov (Reg Rax, stack_offset stack_index) ]
 
 let compile (exp : s_exp) : directive list =
   let directives = compile_exp (-8) exp in
--- lib/interp.ml
+++ lib/interp.ml
@@ -4,34 +4,41 @@ open Util
 
 type value = Number of int | Boolean of bool
 
-let st = Symtab.empty
-
 let int_of_value (v : value) : int =
   match v with Number n -> n | Boolean _ -> failwith "boolean!"
 
-let rec interp_exp (exp : s_exp) : value =
+let rec interp_exp (env : value symtab) (exp : s_exp) : value =
   match exp with
   | Num n -> Number n
   | Sym "false" -> Boolean false
   | Sym "true" -> Boolean true
-  | Lst [ Sym "add1"; l ] -> Number (int_of_value (interp_exp l) + 1)
-  | Lst [ Sym "sub1"; l ] -> Number (int_of_value (interp_exp l) - 1)
+  | Sym var -> Symtab.find var env
+  | Lst [ Sym "let"; Lst [ Lst [ Sym var; e ] ]; e_body ] ->
+      let e_value = interp_exp env e in
+      interp_exp (Symtab.add var e_value env) e_body
+  | Lst [ Sym "add1"; l ] -> Number (int_of_value (interp_exp env l) + 1)
+  | Lst [ Sym "sub1"; l ] -> Number (int_of_value (interp_exp env l) - 1)
   | Lst [ Sym "not"; l ] ->
-      if interp_exp l = Boolean false then Boolean true else Boolean false
+      if interp_exp env l = Boolean false then Boolean true else Boolean false
   | Lst [ Sym "zero?"; l ] ->
-      if interp_exp l = Number 0 then Boolean true else Boolean false
+      if interp_exp env l = Number 0 then Boolean true else Boolean false
   | Lst [ Sym "num?"; l ] -> (
-      match interp_exp l with Number _ -> Boolean true | _ -> Boolean false)
+      match interp_exp env l with
+      | Number _ -> Boolean true
+      | _ -> Boolean false)
   | Lst [ Sym "if"; e_cond; e_then; e_else ] ->
-      let v_cond = interp_exp e_cond in
-      if v_cond = Boolean false then interp_exp e_else else interp_exp e_then
+      let v_cond = interp_exp env e_cond in
+      if v_cond = Boolean false then interp_exp env e_else
+      else interp_exp env e_then
   | Lst [ Sym "+"; e1; e2 ] ->
-      Number (int_of_value (interp_exp e1) + int_of_value (interp_exp e2))
+      Number
+        (int_of_value (interp_exp env e1) + int_of_value (interp_exp env e2))
   | Lst [ Sym "-"; e1; e2 ] ->
-      Number (int_of_value (interp_exp e1) - int_of_value (interp_exp e2))
-  | Lst [ Sym "="; e1; e2 ] -> Boolean (interp_exp e1 = interp_exp e2)
+      Number
+        (int_of_value (interp_exp env e1) - int_of_value (interp_exp env e2))
+  | Lst [ Sym "="; e1; e2 ] -> Boolean (interp_exp env e1 = interp_exp env e2)
   | Lst [ Sym "<"; e1; e2 ] -> (
-      match (interp_exp e1, interp_exp e2) with
+      match (interp_exp env e1, interp_exp env e2) with
       | Number n1, Number n2 -> Boolean (n1 < n2)
       | _ -> raise (Stuck exp))
   | _ -> raise (Stuck exp)
@@ -39,4 +46,5 @@ let rec interp_exp (exp : s_exp) : value =
 let string_of_value (v : value) : string =
   match v with Number n -> string_of_int n | Boolean b -> string_of_bool b
 
-let interp (program : s_exp) : string = interp_exp program |> string_of_value
+let interp (program : s_exp) : string =
+  interp_exp Symtab.empty program |> string_of_value
