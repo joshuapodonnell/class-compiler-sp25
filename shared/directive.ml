8f059729574881375556293e29cefe5ef9148c77
Max Willsey <me@mwillsey.com>
2025-04-17 12:01:46 -0700

    add computedcall directive

--- shared/directive.ml
+++ shared/directive.ml
@@ -2,56 +2,34 @@ type register = Rax | R8 | R9 | R10 | R11 | Rsp | Rdi | Rsi | Rdx | Rbp
 
 let string_of_register ?(byte = false) (reg : register) =
   match (reg, byte) with
-  | Rax, false ->
-      "rax"
-  | Rax, true ->
-      "al"
-  | R8, false ->
-      "r8"
-  | R8, true ->
-      "r8b"
-  | R9, false ->
-      "r9"
-  | R9, true ->
-      "r9b"
-  | R10, false ->
-      "r10"
-  | R10, true ->
-      "r10b"
-  | R11, false ->
-      "r11"
-  | R11, true ->
-      "r11b"
-  | Rsp, false ->
-      "rsp"
-  | Rsp, true ->
-      "rsp"
-  | Rdi, false ->
-      "rdi"
-  | Rdi, true ->
-      "rdi"
-  | Rsi, false ->
-      "rsi"
-  | Rsi, true ->
-      "rsi"
-  | Rdx, false ->
-      "rdx"
-  | Rdx, true ->
-      "rdx"
-  | Rbp, false ->
-      "rbp"
-  | Rbp, true ->
-      "rbp"
+  | Rax, false -> "rax"
+  | Rax, true -> "al"
+  | R8, false -> "r8"
+  | R8, true -> "r8b"
+  | R9, false -> "r9"
+  | R9, true -> "r9b"
+  | R10, false -> "r10"
+  | R10, true -> "r10b"
+  | R11, false -> "r11"
+  | R11, true -> "r11b"
+  | Rsp, false -> "rsp"
+  | Rsp, true -> "rsp"
+  | Rdi, false -> "rdi"
+  | Rdi, true -> "rdi"
+  | Rsi, false -> "rsi"
+  | Rsi, true -> "rsi"
+  | Rdx, false -> "rdx"
+  | Rdx, true -> "rdx"
+  | Rbp, false -> "rbp"
+  | Rbp, true -> "rbp"
 
 type operand = Reg of register | Imm of int | MemOffset of (operand * operand)
 
 let is_register o = match o with Reg _ -> true | _ -> false
 
 let rec string_of_operand ?(byte = false) = function
-  | Reg r ->
-      string_of_register ~byte r
-  | Imm i ->
-      string_of_int i
+  | Reg r -> string_of_register ~byte r
+  | Imm i -> string_of_int i
   | MemOffset (o1, o2) ->
       if byte then
         Printf.sprintf "BYTE [%s + %s]"
@@ -99,6 +77,7 @@ type directive =
   | Push of operand
   | Pop of operand
   | Call of string
+  | ComputedCall of operand
   | Comment of string
 
 let label_name (macos : bool) (label : string) : string =
@@ -110,22 +89,15 @@ let string_of_directive ~macos = function
       Printf.sprintf
         (if macos then "default rel\nglobal _%s" else "global %s")
         l
-  | Extern l ->
-      Printf.sprintf "extern %s" (label_name macos l)
-  | Section l ->
-      Printf.sprintf "\tsection .%s" l
+  | Extern l -> Printf.sprintf "extern %s" (label_name macos l)
+  | Section l -> Printf.sprintf "\tsection .%s" l
   (* labels *)
-  | Label l ->
-      Printf.sprintf "%s:" (label_name macos l)
+  | Label l -> Printf.sprintf "%s:" (label_name macos l)
   (* data *)
-  | DqLabel l ->
-      Printf.sprintf "\tdq %s" (label_name macos l)
-  | DqString l ->
-      Printf.sprintf "\tdq `%s`, 0" (String.escaped l)
-  | DqInt i ->
-      Printf.sprintf "\tdq %d" i
-  | Align i ->
-      Printf.sprintf "align %d" i
+  | DqLabel l -> Printf.sprintf "\tdq %s" (label_name macos l)
+  | DqString l -> Printf.sprintf "\tdq `%s`, 0" (String.escaped l)
+  | DqInt i -> Printf.sprintf "\tdq %d" i
+  | Align i -> Printf.sprintf "align %d" i
   (* instructions *)
   | Mov (dest, src) ->
       Printf.sprintf "\tmov %s, %s" (string_of_operand dest)
@@ -140,12 +112,9 @@ let string_of_directive ~macos = function
   | Sub (dest, src) ->
       Printf.sprintf "\tsub %s, %s" (string_of_operand dest)
         (string_of_operand src)
-  | Div src ->
-      Printf.sprintf "\tidiv QWORD %s" (string_of_operand src)
-  | Mul src ->
-      Printf.sprintf "\timul QWORD %s" (string_of_operand src)
-  | Cqo ->
-      Printf.sprintf "\tcqo"
+  | Div src -> Printf.sprintf "\tidiv QWORD %s" (string_of_operand src)
+  | Mul src -> Printf.sprintf "\timul QWORD %s" (string_of_operand src)
+  | Cqo -> Printf.sprintf "\tcqo"
   | Shl (dest, src) ->
       Printf.sprintf "\tshl %s, %s" (string_of_operand dest)
         (string_of_operand src)
@@ -164,36 +133,22 @@ let string_of_directive ~macos = function
   | Or (dest, src) ->
       Printf.sprintf "\tor %s, %s" (string_of_operand dest)
         (string_of_operand src)
-  | Setz dest ->
-      Printf.sprintf "\tsete %s" (string_of_operand ~byte:true dest)
-  | Setl dest ->
-      Printf.sprintf "\tsetl %s" (string_of_operand ~byte:true dest)
+  | Setz dest -> Printf.sprintf "\tsete %s" (string_of_operand ~byte:true dest)
+  | Setl dest -> Printf.sprintf "\tsetl %s" (string_of_operand ~byte:true dest)
   | LeaLabel (dest, label) ->
       Printf.sprintf "\tlea %s, [%s]" (string_of_operand dest)
         (label_name macos label)
-  | Jmp dest ->
-      Printf.sprintf "\tjmp %s" (label_name macos dest)
-  | Je dest ->
-      Printf.sprintf "\tje %s" (label_name macos dest)
-  | Jne dest ->
-      Printf.sprintf "\tjne %s" (label_name macos dest)
-  | Jl dest ->
-      Printf.sprintf "\tjl %s" (label_name macos dest)
-  | Jnl dest ->
-      Printf.sprintf "\tjnl %s" (label_name macos dest)
-  | Jg dest ->
-      Printf.sprintf "\tjg %s" (label_name macos dest)
-  | Jng dest ->
-      Printf.sprintf "\tjng %s" (label_name macos dest)
-  | ComputedJmp dest ->
-      Printf.sprintf "\tjmp %s" (string_of_operand dest)
-  | Push o ->
-      Printf.sprintf "\tpush %s" (string_of_operand o)
-  | Pop o ->
-      Printf.sprintf "\tpop %s" (string_of_operand o)
-  | Call dest ->
-      Printf.sprintf "\tcall %s" (label_name macos dest)
-  | Ret ->
-      "\tret"
-  | Comment s ->
-      Printf.sprintf "; %s" s
+  | Jmp dest -> Printf.sprintf "\tjmp %s" (label_name macos dest)
+  | Je dest -> Printf.sprintf "\tje %s" (label_name macos dest)
+  | Jne dest -> Printf.sprintf "\tjne %s" (label_name macos dest)
+  | Jl dest -> Printf.sprintf "\tjl %s" (label_name macos dest)
+  | Jnl dest -> Printf.sprintf "\tjnl %s" (label_name macos dest)
+  | Jg dest -> Printf.sprintf "\tjg %s" (label_name macos dest)
+  | Jng dest -> Printf.sprintf "\tjng %s" (label_name macos dest)
+  | ComputedJmp dest -> Printf.sprintf "\tjmp %s" (string_of_operand dest)
+  | Push o -> Printf.sprintf "\tpush %s" (string_of_operand o)
+  | Pop o -> Printf.sprintf "\tpop %s" (string_of_operand o)
+  | Call dest -> Printf.sprintf "\tcall %s" (label_name macos dest)
+  | Ret -> "\tret"
+  | Comment s -> Printf.sprintf "; %s" s
+  | ComputedCall o -> Printf.sprintf "\tcall %s" (string_of_operand o)
